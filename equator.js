f002d1c1511a64030fe97940d8175d827de264e0
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-11-09 12:36:04 +0000

    added solve

--- equator.js
+++ equator.js
@@ -1,5 +1,35 @@
 
 
-let expression = "x^2 + x - 6";
 
-console.log ("Expressing " + expression);
+
+function solve( a, b, c){
+
+     let bsquaredminus4ac = (b ** 2) - (4 * a * c);
+
+     let plusSolution = ( (-b) + Math.sqrt(bsquaredminus4ac) ) / (2 * a );
+
+     let minusSolution = ( (-b) - Math.sqrt(bsquaredminus4ac) ) / (2 * a );
+
+     let sqrt = ( Math.sqrt(bsquaredminus4ac) );
+     let bsqrt = ( (-b) - ( Math.sqrt(bsquaredminus4ac) ));
+
+     return {
+         solved : true,
+         solutionValues: [plusSolution, minusSolution ],
+         error : null
+    };
+
+}
+
+function displaySolution( solution ) {
+    if ( solution.solved){ 
+        console.log ("Solution " + solution.solutionValues);
+    } else {
+        console.log(solution.error);
+    }
+    
+}
+
+
+displaySolution(solve(1, -5, 6) );
+displaySolution(solve(0, -5, 6) );
