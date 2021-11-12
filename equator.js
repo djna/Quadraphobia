6f488fe3d278b6814fb10760bb6053eaeedac1e6
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-11-12 09:02:17 +0000

    use intermediate values for demo

--- equator.js
+++ equator.js
@@ -1,35 +1,77 @@
 
+class Quadratic {
+    
+    constructor( a, b, c ){
+        this.a = a;
+        this.b = b;
+        this.c =c;
+    }
 
+    evaluate(x){
+        let axsquared = this.a * x * x;
+        let bx = this.b * x;
+        let xpart = axsquared + bx;
+        let value = xpart + this.c;
+        return value;
+    }
 
+    solveZero(){
 
-function solve( a, b, c){
+        let bsquaredminus4ac = (this.b ** 2) - (4 * this.a * this.c);
+   
+        let plusSolution = ( (-this.b) + Math.sqrt(bsquaredminus4ac) ) / (2 * this.a );
+   
+        let minusSolution = ( (-this.b) - Math.sqrt(bsquaredminus4ac) ) / (2 * this.a );
+   
+        return {
+            solved : true,
+            solutionValues: [plusSolution, minusSolution ],
+            error : null
+       };
+   }
 
-     let bsquaredminus4ac = (b ** 2) - (4 * a * c);
+   toString(){
+       return this.a + "x^2 + " + this.b + "x + " + this.c;
+   }
+};
 
-     let plusSolution = ( (-b) + Math.sqrt(bsquaredminus4ac) ) / (2 * a );
-
-     let minusSolution = ( (-b) - Math.sqrt(bsquaredminus4ac) ) / (2 * a );
-
-     let sqrt = ( Math.sqrt(bsquaredminus4ac) );
-     let bsqrt = ( (-b) - ( Math.sqrt(bsquaredminus4ac) ));
-
-     return {
-         solved : true,
-         solutionValues: [plusSolution, minusSolution ],
-         error : null
-    };
-
-}
 
 function displaySolution( solution ) {
     if ( solution.solved){ 
         console.log ("Solution " + solution.solutionValues);
     } else {
         console.log(solution.error);
-    }
-    
+    }   
 }
 
+function verifySolution( quadratic, solution ) {
+    if ( solution.solved){ 
 
-displaySolution(solve(1, -5, 6) );
-displaySolution(solve(0, -5, 6) );
+        solution.solutionValues.forEach(value => {
+            console.log( quadratic +  ": x = " + value 
+             + ", evaluates to " + quadratic.evaluate(value));
+        });
+        
+    } else {
+        console.log(solution.error);
+    }   
+}
+
+let goodQuadratic = new Quadratic(1, -5, 6);
+let goodSolution = goodQuadratic.solveZero();
+displaySolution(goodSolution );
+verifySolution(goodQuadratic, goodSolution);
+
+
+
+let badQuadratic = new Quadratic( 0, -5, 6 );
+let badSolution = badQuadratic.solveZero();
+displaySolution(badSolution );
+
+let bigQuadratic = new Quadratic(1, -5e50, 6e100);
+let bigSolution = bigQuadratic.solveZero();
+displaySolution(bigSolution );
+verifySolution(bigQuadratic, bigSolution);
+
+
+
