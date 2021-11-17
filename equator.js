4c8ce7e03d1c265da13d25b91cb93b5d82f74f1d
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-11-17 06:42:55 +0000

    use decimal

--- equator.js
+++ equator.js
@@ -1,27 +1,32 @@
 
+const Decimal = require('decimal.js');
+
 class Quadratic {
     
     constructor( a, b, c ){
-        this.a = a;
-        this.b = b;
-        this.c =c;
+        this.a = new Decimal(a);
+        this.b = new Decimal(b);
+        this.c = new Decimal(c);
     }
 
     evaluate(x){
-        let axsquared = this.a * x * x;
-        let bx = this.b * x;
-        let xpart = axsquared + bx;
-        let value = xpart + this.c;
+        let axsquared = this.a.times(x).times(x);
+        let bx = this.b.times(x);
+        let xpart = axsquared.add(bx);
+        let value =  xpart.add(this.c);
         return value;
     }
 
     solveZero(){
 
-        let bsquaredminus4ac = (this.b ** 2) - (4 * this.a * this.c);
+        let bsquaredminus4ac = this.b.toPower(2).minus(
+             this.a.times(4).times(this.c) );
    
-        let plusSolution = ( (-this.b) + Math.sqrt(bsquaredminus4ac) ) / (2 * this.a );
+        let plusSolution = this.b.negated().add( bsquaredminus4ac.sqrt() ) 
+                     .dividedBy(this.a.times(2) ) ;
    
-        let minusSolution = ( (-this.b) - Math.sqrt(bsquaredminus4ac) ) / (2 * this.a );
+        let minusSolution = this.b.negated().minus( bsquaredminus4ac.sqrt() ) 
+                    .dividedBy(this.a.times(2) ) ;
    
         return {
             solved : true,
@@ -73,5 +78,52 @@ let bigSolution = bigQuadratic.solveZero();
 displaySolution(bigSolution );
 verifySolution(bigQuadratic, bigSolution);
 
+let aNan = Number("x");
+let anInfinity = 1 /0;
+let minusInfinity = -1 / 0;
+let unspecified = 0 / 0;
+let point1 = 0.1;
+let point2 = 0.2;
+let point3 = 0.3;
+
+let aBadNumber = Number("x");
+let bBadNumber = Number("x");
+let badEqualsBad = aBadNumber === bBadNumber;
+console.log ( "" + aBadNumber + " === " + aBadNumber + " " + badEqualsBad );
+
+console.log( point1 + " + " +  point2 + " === " + point3 + ": " + ( (point1 + point2) === point3));
+console.log( " infinities " + anInfinity + ", " + minusInfinity);
+
+console.log( "infinity equals " + (anInfinity === anInfinity) );
+console.log( "nan equals " + (aNan === aNan) );
+
+//let maxi = Number.MAX_SAFE_INTEGER - 1;
+
+/*
+for ( let maxi = 9007199254740990; 
+          maxi <= 9007199254740993;
+          maxi ++ ) {
+
+             console.log("maxi = " + maxi );
+
+          }
+*/
+
+let oneMoreThanMax = Number.MAX_VALUE + 1;
+console.log("1", oneMoreThanMax);
+
+let lotsMoreThanMax = Number.MAX_VALUE + 9.979201e291;
+console.log("lots", lotsMoreThanMax);
+
+let wayMoreThanMax = Number.MAX_VALUE + 9.979202e291;
+console.log("way", wayMoreThanMax);
+
+/*
+for (let i = (Number.MAX_VALUE/10); i < Number.MAX_VALUE - 1; i++) {
+   console.log ("i = " + i);
+}
+*/
+
+let x = 7777n;
 
 
