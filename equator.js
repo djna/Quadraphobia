5641146dbb28d50a5e04294cb811d6e182ef63c4
David Artus <djna01@gmail.com>
2022-11-23 09:32:51 +0000

    Check for valid quadratic Delta depending on size of value

--- equator.js
+++ equator.js
@@ -17,7 +17,23 @@ class Quadratic {
 
     solveZero(){
 
+        if (this.a == 0 ){
+            return {
+                solved : false,
+                solutionValues: null,
+                error : "This is not a valid quadratic, 'a' is zero"
+           };
+        }
+
         let bsquaredminus4ac = (this.b ** 2) - (4 * this.a * this.c);
+
+        if ( bsquaredminus4ac < 0 ){
+            return {
+                solved : false,
+                solutionValues: null,
+                error : "This quaratic has no real solutions, 'b squared - 4ac' is negative"
+           };
+        }
    
         let plusSolution = ( (-this.b) + Math.sqrt(bsquaredminus4ac) ) / (2 * this.a );
    
@@ -46,28 +62,43 @@ function displaySolution( solution ) {
 
 function verifySolution( quadratic, solution ) {
     if ( solution.solved){ 
-
+       
         solution.solutionValues.forEach(value => {
-            console.log( quadratic +  ": x = " + value 
-             + ", evaluates to " + quadratic.evaluate(value));
+
+            let closeEnoughToZero = Math.abs(value * value / 1e15);
+
+            let evaluation = quadratic.evaluate(value);
+            let validSolution = (evaluation <= closeEnoughToZero )
+            if ( validSolution ){
+                console.log( quadratic +  ": x = " + value 
+                            + ", evaluates to " + evaluation + ", which is close enough to zero");
+                return true;
+            } else {
+                console.log( quadratic +  ": x = " + value 
+                            + ", evaluates to " + evaluation + ", which is not nearly zero ");
+                return false;
+            }
         });
         
     } else {
         console.log(solution.error);
+        return false;
     }   
 }
 
+console.log("Good Quadratic");
 let goodQuadratic = new Quadratic(1, -5, 6);
 let goodSolution = goodQuadratic.solveZero();
 displaySolution(goodSolution );
 verifySolution(goodQuadratic, goodSolution);
 
 
-
+console.log("Bad Quadratic");
 let badQuadratic = new Quadratic( 0, -5, 6 );
 let badSolution = badQuadratic.solveZero();
 displaySolution(badSolution );
 
+console.log("Big Quadratic");
 let bigQuadratic = new Quadratic(1, -5e50, 6e100);
 let bigSolution = bigQuadratic.solveZero();
 displaySolution(bigSolution );
